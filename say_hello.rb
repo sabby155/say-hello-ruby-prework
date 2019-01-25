1a849f722fcedeec6b1293c0489e0346c7a4769d
Sabrina Chowdhury <nebula155@gmail.com>
2019-01-25 21:10:45 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
 # Build your say_hello method here
 
 def intro(name="Student")
-  "Hello, #{name}, welcome to your first day at school!"
+  puts "Hello, #{name}, welcome to your first day at school!"
 end   
